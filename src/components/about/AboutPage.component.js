fa20eb22114b76f0b7307a69fcd7cb4c162512ea
Jim Hoy <jim.hoy@gmail.com>
2017-01-31 14:53:53 -0500

    initial commit

---
+++ src/components/about/AboutPage.component.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class AboutPage extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1>About</h1>
+                <p>This is a react.js implementation (with lots of other technologies too!) of the once-popular game
+                    called 'MetaSquares'.
+                </p>
+            </div>
+        );
+    }
+}
+
+export default AboutPage;
